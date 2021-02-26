6dd7569491b4edf6ed7620c1f6f9aca6675aff74
dudas-gabriel <dudas.gaby655@yahoo.com>
2021-02-26 20:50:17 +0100

    est ok

---
+++ Assets/Script/characterMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    CharacterController ChrController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float Gravity = 20.0f;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChrController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ChrController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        else
+        {
+
+        }
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            float xScale = Mathf.Abs(transform.localScale.x);
+
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                transform.localScale = new Vector3(-xScale, transform.localScale.y, transform.localScale.z);
+
+            }
+            else
+            {
+                transform.localScale = new Vector3(xScale, transform.localScale.y, transform.localScale.z);
+            }
+        }
+
+        moveDirection.y -= Gravity * Time.deltaTime;
+
+        ChrController.Move(moveDirection * Time.deltaTime);
+    }
+}
